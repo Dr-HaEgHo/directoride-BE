c892775cca57a0eec75a1cb31ecd95228ffb6c7d
Teem1345 <awogbuyitimothy@gmail.com>
2023-07-24 16:03:32 +0100

    removed cors

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const heroSliderImages = require('./dataset/heroSliderImages');
 const notifications = require('./dataset/notifications');
 const pricelist = require('./dataset/pricelist');
 const faqs = require('./dataset/faqs');
+// const cors = require('cors')
 
 const server = express();
+// server.use(cors({
+//     origin: "*"
+// }));
 
 const HOST = 'localhost'
-const PORT = 8888;
+const PORT = process.env.PORT || 8888;
 
 server.get('/', (req, res) => {
-    res.send('Welcome to the Todo app Server')
+    res.send('Welcome to the Directoride app Server')
 })
 
 server.get('/riders', (req, res) => {
@@ -20,7 +24,7 @@ server.get('/riders', (req, res) => {
 
 server.get('/hero-slides', (req, res) => {
     res.status(200).json(heroSliderImages)
-})
+}) 
 
 server.get('/notifications', (req, res) => {
     res.status(200).json(notifications)
@@ -33,7 +37,5 @@ server.get('/pricelist', (req, res) => {
 server.get('/faqs', (req, res) => {
     res.status(200).send(faqs)
 })
-
-
 
 server.listen(PORT, () => console.log(`server Running at: ${HOST}:${PORT}`));
